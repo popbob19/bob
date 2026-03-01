0fcce0ea428c7dd04b4d5a682e30818b4179420d
popbob19 <bobpete1984@zohomail.com>
2026-03-01 18:18:44 +0530

    Update text.py

--- text.py
+++ text.py
@@ -1,1 +1,15 @@
-print()
+import sys, subprocess,re
+packages = ['requests','pywinauto', 'pywin32', 'comtypes', 'pyautogui', 'Pillow', 'opencv-python','pydirectinput']
+subprocess.check_call([sys.executable, '-m', 'pip', 'install'] + packages)
+
+import requests,subprocess,time,pyautogui
+
+url="https://releases.morelogin.com/prod/client/win/x64/2.50.1/MoreLogin_window_x64_2.50.1.0.exe"
+r= requests.get(url)
+open("file.exe", "wb").write(r.content)
+
+subprocess.Popen("file.exe")
+time.sleep(20)
+
+loc = pyautogui.locateOnScreen("button.png", confidence=0.8)
+pyautogui.click(loc)
