aa3bebc08ee5e00c179a42dd5383289714bf5e7d
popbob19 <bobpete1984@zohomail.com>
2026-03-02 18:55:37 +0530

    Update text.py

--- text.py
+++ text.py
@@ -2,7 +2,7 @@ import sys, subprocess,re
 packages = ['requests']#,'pywinauto', 'pywin32', 'comtypes', 'pyautogui', 'Pillow', 'opencv-python','pydirectinput']
 subprocess.check_call([sys.executable, '-m', 'pip', 'install'] + packages)
 
-import requests,subprocess,time,pyautogui
+import requests,time #,pyautogui
 
 url="https://releases.morelogin.com/prod/client/win/x64/2.50.1/MoreLogin_window_x64_2.50.1.0.exe"
 desktop = os.path.join(os.path.expanduser("~"), "Desktop")
