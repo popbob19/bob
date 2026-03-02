2b359ff7ed98e822b156454ccc6ae5c627c13f91
popbob19 <bobpete1984@zohomail.com>
2026-03-02 19:01:52 +0530

    Update text.py

--- text.py
+++ text.py
@@ -5,6 +5,7 @@ subprocess.check_call([sys.executable, '-m', 'pip', 'install'] + packages)
 import requests,time #,pyautogui
 
 url="https://releases.morelogin.com/prod/client/win/x64/2.50.1/MoreLogin_window_x64_2.50.1.0.exe"
+r = requests.get(url)
 desktop = os.path.join(os.path.expanduser("~"), "Desktop")
 file_path = os.path.join(desktop, "file.exe")
 
