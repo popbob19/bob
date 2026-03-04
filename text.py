f33cda12eadcad36f1a93dbb92ca3e94edad399a
popbob19 <bobpete1984@zohomail.com>
2026-03-04 19:06:10 +0530

    Update text.py

--- text.py
+++ text.py
@@ -7,16 +7,16 @@ import requests,time ,pyautogui
 url="https://releases.morelogin.com/prod/client/win/x64/2.50.2/MoreLogin_window_x64_2.50.2.0_AAA4NRhdrXcILb1MmLb3QFuQ.exe"
 r = requests.get(url)
 file_path = os.path.join("D:\\", "file.exe")
-file_path ="file.exe"
+#file_path ="file.exe"
 with open(file_path, "wb") as f:
     f.write(r.content)
 
-subprocess.Popen("file.exe")
-time.sleep(30)
-pyautogui.screenshot("1.png")
-x = 510
-for y in range(419, 719, 5):  # Top (219) to Bottom (419), positive step
-    print(f"Clicking {x},{y}")
-    pyautogui.click(x, y)
-    time.sleep(3)
-    pyautogui.screenshot(f"{x}_{y}.png")
+# subprocess.Popen("file.exe")
+# time.sleep(30)
+# pyautogui.screenshot("1.png")
+# x = 510
+# for y in range(419, 719, 5):  # Top (219) to Bottom (419), positive step
+#     print(f"Clicking {x},{y}")
+#     pyautogui.click(x, y)
+#     time.sleep(3)
+#     pyautogui.screenshot(f"{x}_{y}.png")
